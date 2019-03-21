3d131133676ae5b2b12d24bdd2daff3e7114d428
乔丹 <qiaodan@qiaodandemacbook-pro.local>
2019-03-21 11:42:39 +0800

    Initial Commit

---
+++ PlanetTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlanetTest
+//
+//  Created by 乔丹 on 2019/3/21.
+//  Copyright © 2019 乔丹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlanetTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlanetTest
+//
+//  Created by 乔丹 on 2019/3/21.
+//  Copyright © 2019 乔丹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
